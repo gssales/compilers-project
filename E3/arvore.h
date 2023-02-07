560c738a538e8cf60280e95299247b144c10ed04
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2023-02-07 18:12:48 -0300

    arvore

--- E3/arvore.h
+++ E3/arvore.h
@@ -1,40 +1,21 @@
 #ifndef _ARVORE_H_
 #define _ARVORE_H_
 
-typedef struct asd_tree {
-  char *label;
-  int number_of_children;
-  struct asd_tree **children;
-} asd_tree_t;
+#include "main.h"
 
-/*
- * Função asd_new, cria um nó sem filhos com o label informado.
- */
-asd_tree_t *asd_new(const char *label);
+typedef struct node node_t;
+struct node {
+	char *label;
+	value_t *value;
+	int count_children;
+	node_t **children;
+};
 
-/*
- * Função asd_tree, libera recursivamente o nó e seus filhos.
- */
-void asd_free(asd_tree_t *tree);
-
-/*
- * Função asd_add_child, adiciona child como filho de tree.
- */
-void asd_add_child(asd_tree_t *tree, asd_tree_t *child);
-
-/*
- * Função asd_print, imprime recursivamente a árvore.
- */
-void asd_print(asd_tree_t *tree);
-
-/*
- * Função asd_print_graphviz, idem, em formato DOT
- */
-void asd_print_graphviz (asd_tree_t *tree);
-
+node_t* create_leaf(char* label, value_t *value);
+node_t* create_node(char* label);
+void add_child(node_t *node, node_t *child);
 /* função usada na main */
 void libera(void* root);
 /* função usada na main */
-void exporta(void* root);
-
+// void exporta(void* root);
 #endif //_ARVORE_H_
