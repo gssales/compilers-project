a6e37c985ea792a852a692dfb82127374a375b48
JChagastelles <jchagastelles@gmail.com>
2023-02-26 12:32:19 -0300

    print valor_lexico alinhado

--- E4/valor_lexico.c
+++ E4/valor_lexico.c
@@ -2,32 +2,32 @@
 
 char* tk_type_to_string(int tk_type) {
   switch(tk_type) {
-    case 0:                 return "CHAR_ESPECIAL";     break;
-    case TK_PR_INT:         return "TK_PR_INT";         break;
-    case TK_PR_FLOAT:       return "TK_PR_FLOAT";       break;
-    case TK_PR_BOOL:        return "TK_PR_BOOL";        break;
-    case TK_PR_CHAR:        return "TK_PR_CHAR";        break;
-    case TK_PR_IF:          return "TK_PR_IF";          break;
-    case TK_PR_THEN:        return "TK_PR_THEN";        break;
-    case TK_PR_ELSE:        return "TK_PR_ELSE";        break;
-    case TK_PR_WHILE:       return "TK_PR_WHILE";       break;
-    case TK_PR_INPUT:       return "TK_PR_INPUT";       break;
-    case TK_PR_OUTPUT:      return "TK_PR_OUTPUT";      break;
-    case TK_PR_RETURN:      return "TK_PR_RETURN";      break;
-    case TK_PR_FOR:         return "TK_PR_FOR";         break;
-    case TK_OC_LE:          return "TK_OC_LE";          break;
-    case TK_OC_GE:          return "TK_OC_GE";          break;
-    case TK_OC_EQ:          return "TK_OC_EQ";          break;
-    case TK_OC_NE:          return "TK_OC_NE";          break;
-    case TK_OC_AND:         return "TK_OC_AND";         break;
-    case TK_OC_OR:          return "TK_OC_OR";          break;
-    case TK_LIT_INT:        return "TK_LIT_INT";        break;
-    case TK_LIT_FLOAT:      return "TK_LIT_FLOAT";      break;
-    case TK_LIT_FALSE:      return "TK_LIT_FALSE";      break;
-    case TK_LIT_TRUE:       return "TK_LIT_TRUE";       break;
-    case TK_LIT_CHAR:       return "TK_LIT_CHAR";       break;
+    case 0:                 return "CHAR_ESPECIAL   ";     break;
+    case TK_PR_INT:         return "TK_PR_INT       ";         break;
+    case TK_PR_FLOAT:       return "TK_PR_FLOAT     ";       break;
+    case TK_PR_BOOL:        return "TK_PR_BOOL      ";        break;
+    case TK_PR_CHAR:        return "TK_PR_CHAR      ";        break;
+    case TK_PR_IF:          return "TK_PR_IF        ";          break;
+    case TK_PR_THEN:        return "TK_PR_THEN      ";        break;
+    case TK_PR_ELSE:        return "TK_PR_ELSE      ";        break;
+    case TK_PR_WHILE:       return "TK_PR_WHILE     ";       break;
+    case TK_PR_INPUT:       return "TK_PR_INPUT     ";       break;
+    case TK_PR_OUTPUT:      return "TK_PR_OUTPUT    ";      break;
+    case TK_PR_RETURN:      return "TK_PR_RETURN    ";      break;
+    case TK_PR_FOR:         return "TK_PR_FOR       ";         break;
+    case TK_OC_LE:          return "TK_OC_LE        ";          break;
+    case TK_OC_GE:          return "TK_OC_GE        ";          break;
+    case TK_OC_EQ:          return "TK_OC_EQ        ";          break;
+    case TK_OC_NE:          return "TK_OC_NE        ";          break;
+    case TK_OC_AND:         return "TK_OC_AND       ";         break;
+    case TK_OC_OR:          return "TK_OC_OR        ";          break;
+    case TK_LIT_INT:        return "TK_LIT_INT      ";        break;
+    case TK_LIT_FLOAT:      return "TK_LIT_FLOAT    ";      break;
+    case TK_LIT_FALSE:      return "TK_LIT_FALSE    ";      break;
+    case TK_LIT_TRUE:       return "TK_LIT_TRUE     ";       break;
+    case TK_LIT_CHAR:       return "TK_LIT_CHAR     ";       break;
     case TK_IDENTIFICADOR:  return "TK_IDENTIFICADOR";  break;
-    case TK_ERRO:           return "TK_ERRO";           break;
+    case TK_ERRO:           return "TK_ERRO         ";           break;
   }
 }
 
